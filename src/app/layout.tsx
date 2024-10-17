441285c0d0e8346e9c7f08694394edee01124004
BestTTTA <bst12546@gmail.com>
2024-10-18 06:10:14 +0700

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Noto_Sans_Thai } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["200"], 
-});
 
 const noto_sans_thai = Noto_Sans_Thai({
   subsets: ["latin"],
